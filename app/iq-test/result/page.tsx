8eddd1fbfc22880c737a8241b45f68ed2325abbd
mohammed-max20 <al.abdalimu@gmail.com>
2026-05-21 02:37:37 +0300

    Added main category tests

--- app/iq-test/result/page.tsx
+++ app/iq-test/result/page.tsx
@@ -82,7 +82,7 @@ export default function ResultPage() {
             href="/"
             className="bg-[#21262d] border border-[#30363d] hover:border-blue-500 transition px-6 py-4 rounded-2xl text-lg font-bold"
           >
-            Home Page
+            Home
           </Link>
         </div>
       </div>
