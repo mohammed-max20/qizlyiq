786d5c67259ec9dc12ce626c4f582f16c0f5d126
mohammed-max20 <al.abdalimu@gmail.com>
2026-05-21 21:19:14 +0300

    Add AdSense

--- app/layout.tsx
+++ app/layout.tsx
@@ -64,6 +64,12 @@ export default function RootLayout({
           `}
         </Script>
 
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6492988602727684"
+          crossOrigin="anonymous"
+        />
+
         {children}
       </body>
     </html>
