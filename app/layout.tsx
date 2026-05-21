ddab4055a28323eb4804725ed71a330b498fcd19
mohammed-max20 <al.abdalimu@gmail.com>
2026-05-21 21:49:47 +0300

    fix tests links

--- app/layout.tsx
+++ app/layout.tsx
@@ -64,13 +64,13 @@ export default function RootLayout({
           `}
         </Script>
 
+        {children}
+
         <Script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6492988602727684"
           crossOrigin="anonymous"
         />
-
-        {children}
       </body>
     </html>
   );
